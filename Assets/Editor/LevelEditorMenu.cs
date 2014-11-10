f66200158ad4e69b6261cfe0c603bb1eea254f21
Polydash <polydash.dev@gmail.com>
2014-11-10 10:55:17 +0100

    Started to work on a new Level Editor script
    It will now display in a window editor and allow user to choose between
    tiles to create a level.

---
+++ Assets/Editor/LevelEditorMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class LevelEditorMenu : EditorWindow
+{
+	Color m_color = Color.red;
+
+	[MenuItem ("Tools/Level Editor")]
+	static void Init()
+	{
+		LevelEditorMenu window = EditorWindow.GetWindow<LevelEditorMenu>();
+		window.title = "Level Editor";
+	}
+
+	private void OnEnable()
+	{
+	}
+
+	private void OnGUI()
+	{
+		Event evt = Event.current;
+
+		if(evt.type == EventType.mouseDown)
+		{
+			m_color = Color.blue;
+		}
+		else if(evt.type == EventType.mouseUp)
+		{
+			m_color = Color.red;
+		}
+
+		EditorGUI.DrawRect(new Rect(0, 0, evt.mousePosition.x, evt.mousePosition.y), m_color);
+		Repaint();
+	}
+}
