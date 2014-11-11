bca8f6d01e45eefaeecae2147476e13acb7b02bf
Guillaume Pastor <pastor.guill@gmail.com>
2014-11-11 14:02:15 +0100

    Player now have IDs
    *Added a prefab playersManager to manage the players IDs
    *Added Script PlayerID in the prefab player which contains their IDs
    *ID will be useful to manage multiple controllers inputs

---
+++ Assets/Scripts/Player/PlayerID.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerID : MonoBehaviour {
+
+
+    private PlayerManager m_scriptPM;
+    private int m_playerID = 0;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_scriptPM = GameObject.Find("PlayersManager").GetComponent<PlayerManager>();
+        m_playerID = m_scriptPM.GetID();//Get the id
+        if(m_playerID == 0)//If the ID is wrong, delete the gameobject
+        {
+            Object.Destroy(gameObject);
+        }
+	}
+
+    /*
+     * This function return the ID of the player. Useful for the inputs.
+     */
+    public int GetPlayerID()
+    {
+        return m_playerID;
+    }
+
+
+    //When the player is destroy, it return its ID to the player manager to notify there is a new slot disponible
+    void OnDestroy()
+    {
+        m_scriptPM.GetIDBack(m_playerID);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    //Number of player Max
+    private const int m_maxPlayers = 4;
+
+    private bool[] m_playersIDs = new bool[m_maxPlayers];
+
+    void Awake()
+    {
+       for(int i = 0; i < m_maxPlayers; ++i)
+       {
+           m_playersIDs[i] = false;//Put everything to false
+       }
+    }
+
+
+    /*
+     * This function returns an ID if there is one disponible, otherwise, it returns 0
+     * Called in the script PlayerID in the player prefab
+     */
+    public int GetID()
+    {
+        for (int i = 0; i < m_maxPlayers; ++i)
+        {
+           if(!m_playersIDs[i])//If there is some place
+           {
+               m_playersIDs[i] = true;
+
+               return i + 1;//Return the id of the player
+           }
+        }
+
+        return 0;//if there is no more place
+    }
+
+
+    /*
+    * This function retrieves an ID put in the parameter
+    * Called in the script PlayerID in the function OnDestroy()
+    */
+    public void GetIDBack(int ID)
+    {
+        if(ID > 0 && ID <= m_maxPlayers)
+        {
+            m_playersIDs[ID - 1] = false;
+        }
+    }
+
+    public int GetMaxPlayer()
+    {
+        return m_maxPlayers;
+    }
+}
