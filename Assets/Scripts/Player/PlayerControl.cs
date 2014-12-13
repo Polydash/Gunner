5620a94d9294b8dde9c465d646fa5bb31f0b3374
Polydash <polydash.dev@gmail.com>
2014-12-13 18:29:06 +0100

    Nerfed rebound (again)

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -557,9 +557,9 @@ public class PlayerControl : MonoBehaviour
 		{
 			if(Mathf.Abs(collision.contacts[i].normal.x) > 0.9f && Mathf.Abs(collision.contacts[i].normal.y) < 0.1f)
 			{
-				if(Mathf.Abs(m_horizontalVelocity) > m_maxVelX/2.0f)
+				if(Mathf.Abs(m_horizontalVelocity) > m_maxVelX/4.0f)
 				{
-					rigidbody2D.velocity = new Vector2(-m_horizontalVelocity/2.0f, rigidbody2D.velocity.y);
+					rigidbody2D.velocity = new Vector2(-m_horizontalVelocity/3.0f, rigidbody2D.velocity.y);
 				}
 			}
 		}
