8717a5c203d7cefbb7b1479ae8e8a0913a474b1e
Polydash <polydash.dev@gmail.com>
2014-12-14 12:31:46 +0100

    Fixed animation problem
    There was a problem where the player would display a jumping animation
    instead of a grounded one.

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -568,6 +568,7 @@ public class PlayerControl : MonoBehaviour
 		rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,
 		                                   Mathf.Max(rigidbody2D.velocity.y, -m_maxGravity));
 
+		//Save horizontal velocity for wall rebound
 		m_horizontalVelocity = rigidbody2D.velocity.x;
 	}
 	
@@ -581,21 +582,34 @@ public class PlayerControl : MonoBehaviour
 				//If the player is not ascending
 				if(rigidbody2D.velocity.y <= 0.0f)
 				{
-					//Player is on the ground
 					m_isGrounded = true;
 					rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
 				}
+
+				//Rebound
+				if(Mathf.Abs(collision.contacts[i].normal.x) > 0.9f && Mathf.Abs(collision.contacts[i].normal.y) < 0.1f)
+				{
+					if(Mathf.Abs(m_horizontalVelocity) > m_maxVelX/4.0f)
+					{
+						rigidbody2D.velocity = new Vector2(-m_horizontalVelocity/2.0f, rigidbody2D.velocity.y);
+					}
+				}
 			}
 		}
+	}
 
-		//Rebound
+	private void OnCollisionStay2D(Collision2D collision)
+	{
 		for(int i=0; i<collision.contacts.Length; ++i)
 		{
-			if(Mathf.Abs(collision.contacts[i].normal.x) > 0.9f && Mathf.Abs(collision.contacts[i].normal.y) < 0.1f)
+			//If player gets on the ground
+			if(collision.contacts[i].normal == new Vector2(0.0f, 1.0f))
 			{
-				if(Mathf.Abs(m_horizontalVelocity) > m_maxVelX/4.0f)
+				//If the player is not ascending
+				if(rigidbody2D.velocity.y <= 0.0f)
 				{
-					rigidbody2D.velocity = new Vector2(-m_horizontalVelocity/2.0f, rigidbody2D.velocity.y);
+					m_isGrounded = true;
+					rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
 				}
 			}
 		}
