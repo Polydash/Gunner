e0ad940ae72e0b47fb18c72b6a62c6216f45d302
Polydash <polydash.dev@gmail.com>
2014-12-14 14:06:09 +0100

    Added prototype chains

---
+++ Assets/Scripts/ChainControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainControl : MonoBehaviour
+{
+	Transform[] m_chains;
+
+	private void Start()
+	{
+		Time.timeScale = 0.3f;
+
+		m_chains = new Transform[3];
+
+		m_chains[0] = transform.GetChild(0);
+		m_chains[1] = transform.GetChild(1);
+		m_chains[2] = transform.GetChild(2);
+	}
+
+	private void Update()
+	{
+		m_chains[0].localPosition = -transform.localPosition / 2.0f;
+		m_chains[1].localPosition = -2 * transform.localPosition / 2.0f;
+		m_chains[2].localPosition = -3 * transform.localPosition / 2.0f;
+	}
+}
