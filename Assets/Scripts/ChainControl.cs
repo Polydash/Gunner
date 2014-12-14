e41c0d63fa3df402603c441e54b0c45a8ac96993
Polydash <polydash.dev@gmail.com>
2014-12-14 15:13:34 +0100

    Prototyped chains updated

--- Assets/Scripts/ChainControl.cs
+++ Assets/Scripts/ChainControl.cs
@@ -5,6 +5,8 @@ public class ChainControl : MonoBehaviour
 {
 	Transform[] m_chains;
 
+	public Vector2 m_centerOffset;
+
 	private void Start()
 	{
 		m_chains = new Transform[3];
@@ -16,8 +18,20 @@ public class ChainControl : MonoBehaviour
 
 	private void Update()
 	{
-		m_chains[0].localPosition = -transform.localPosition / 2.0f;
-		m_chains[1].localPosition = -2 * transform.localPosition / 2.0f;
-		m_chains[2].localPosition = -3 * transform.localPosition / 2.0f;
+		Vector2 playerPos = transform.parent.position;
+		playerPos += m_centerOffset;
+		Vector2 gloveDist = new Vector2(transform.position.x, transform.position.y) - playerPos;
+
+		for(int i=0; i<m_chains.Length; ++i)
+		{
+			m_chains[i].position = playerPos + ((float)i+1)/(m_chains.Length+1) * gloveDist;
+		}
+	}
+
+	public void SetVisible(bool enable)
+	{
+		m_chains[0].renderer.enabled = enable;
+		m_chains[1].renderer.enabled = enable;
+		m_chains[2].renderer.enabled = enable;
 	}
 }
