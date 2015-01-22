e3a0ad80c6eb422a869e78cd023d6702ffa6529b
Polydash <polydash.dev@gmail.com>
2015-01-22 12:38:45 +0100

    Modified FX animation control

--- Assets/Scripts/FXControl.cs
+++ Assets/Scripts/FXControl.cs
@@ -6,17 +6,18 @@ public class FXControl : MonoBehaviour
 	private float m_elapsedTime = 0.0f;
 	public  float m_maxTime;
 
+    private Animator m_animator;
+
 	void Start()
-	{		
+	{
+        m_animator = GetComponent<Animator>();
 	}
 
 	void Update()
 	{
-		if(m_elapsedTime > m_maxTime)
+		if(m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
 		{
 			GameObject.Destroy(gameObject);
 		}
-
-		m_elapsedTime += Time.deltaTime;
 	}
 }
